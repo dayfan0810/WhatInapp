00aaa752963a8f85f18c29b9d700967f3f5396a7
dayfan0810 <mr_day@163.com>
2016-11-12 22:31:35 +0800

    11

--- app/src/main/java/com/hope/com/whatinapp/MyApp.java
+++ app/src/main/java/com/hope/com/whatinapp/MyApp.java
@@ -1,16 +1,24 @@
 package com.hope.com.whatinapp;
 
+import android.app.Activity;
 import android.app.Application;
 
+import com.hope.com.whatinapp.utils.SharedPreferencesUtil;
+
 import org.xutils.x;
+
+import java.util.ArrayList;
 
 /**
  * Created by Day on 2016/11/5.
  */
 
 public class MyApp extends Application {
+    public static ArrayList<Activity> actitivitys = new ArrayList<>();
+    public SharedPreferencesUtil sharedPreferencesUtil;
     @Override
     public void onCreate() {
+        sharedPreferencesUtil = SharedPreferencesUtil.getInstance(this);
         super.onCreate();
         x.Ext.init(this);
     }
--- app/src/main/java/com/hope/com/whatinapp/activity/BaseActivity.java
+++ app/src/main/java/com/hope/com/whatinapp/activity/BaseActivity.java
@@ -1,119 +1,55 @@
 package com.hope.com.whatinapp.activity;
 
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.Color;
-import android.os.Build;
+import android.content.res.TypedArray;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.hope.com.whatinapp.R;
+import com.hope.com.whatinapp.utils.SharedPreferencesUtil;
 
 public class BaseActivity extends AppCompatActivity {
+    public static int color = 0;
 
-    private int mColor = -1;
+    public static int[] style = {R.style.AppTheme, R.style.AppThemeMaterial, R.style.DarkMode};
+    public static String[] strings = {"material theme", "dark mode"};
+    private SharedPreferencesUtil sharedPreferencesUtil;
+    public static final int TYPE_THEME_MATERIAL = 1;
+    public static final int TYPE_THEME_DARK = 2;
 
-    public BaseActivity() {
-    }
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
 
-    public BaseActivity(int color) {
-        super();
-        mColor = color;
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        sharedPreferencesUtil = SharedPreferencesUtil.getInstance(this);
+        setTheme();
         super.onCreate(savedInstanceState);
-        StatusBarCompat.compat(this, ContextCompat.getColor(this, mColor));
 
     }
 
-    public static class StatusBarCompat {
+    private void setTheme() {
+        sharedPreferencesUtil.saveInt("theme", BaseActivity.TYPE_THEME_MATERIAL);
+        getColor();
+        setTheme();
+    }
 
-        private static final int INVALID_VAL = -1;
-        private static final int COLOR_DEFAULT = Color.parseColor("#20000000");
+    private void getColor() {
+        TextView s = new TextView(this);
+        TypedArray array = getTheme().obtainStyledAttributes(new int[]{
+                android.R.attr.colorPrimary,
+                android.R.attr.colorPrimaryDark,
+        });
 
-        public static void compat(Activity activity, int statusColor) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                if (statusColor != INVALID_VAL) {
-                    activity.getWindow().setStatusBarColor(statusColor);
-                }
-                return;
-            }
+//        color = array.getColor(0, getResources().getColor(R.color.blue_theme));
+        int textColor = array.getColor(1, 0xFF00FF);
 
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT
-                    && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-                int color = COLOR_DEFAULT;
-                ViewGroup contentView = (ViewGroup) activity.findViewById(android.R.id.content);
+        array.recycle();
 
-                if (statusColor != INVALID_VAL) {
-                    color = statusColor;
-                }
-
-                View statusBarView = new View(activity);
-                ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                        getStatusBarHeight(activity));
-                statusBarView.setBackgroundColor(color);
-
-//                contentView.addView(statusBarView, lp);
-                contentView.addView(statusBarView, 0, lp);
-            }
-        }
-
-        public static void compat(Activity activity) {
-            compat(activity, INVALID_VAL);
-        }
-
-        public static int getStatusBarHeight(Context context) {
-            int result = 0;
-
-            int resourceId = context.getResources().getIdentifier("status_bar_height",
-                    "dimen", "android");
-
-            if (resourceId > 0) {
-                result = context.getResources().getDimensionPixelSize(resourceId);
-            }
-
-            return result;
-        }
     }
 }
-
-//public class BaseActivity extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//    }
-//
-//    protected void setImmerseLayout(View view) {
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-//            Window window = getWindow();
-//                /*window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
-//                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);*/
-//            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-//
-//            int statusBarHeight = getStatusBarHeight(this.getBaseContext());
-//            view.setPadding(0, statusBarHeight, 0, 0);
-//        }
-//    }
-//
-//    /**
-//     * 用于获取状态栏的高度。 使用Resource对象获取（推荐这种方式）
-//     *
-//     * @return 返回状态栏高度的像素值。
-//     */
-//    public static int getStatusBarHeight(Context context) {
-//        int result = 0;
-//        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen",
-//                "android");
-//        if (resourceId > 0) {
-//            result = context.getResources().getDimensionPixelSize(resourceId);
-//        }
-//        return result;
-//    }
-//
-//}
